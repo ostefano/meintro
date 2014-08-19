41eebe4539ed4a66f846f29a3edd02ca61a401ed
Stefano Ortolani <ostefano@gmail.com>
2014-08-19 14:25:31 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meintro
+{
+    class Program
+    {
+
+        private int SHM_PROCESS_SIZE = 2956696;
+        private int SHM_THREAD_SIZE = 46192;
+        private int SHM_DLL = 176;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
