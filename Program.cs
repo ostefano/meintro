3a41e56162d40fee2472f920185133aea3b07e6c
Stefano Ortolani <ostefano@gmail.com>
2014-09-04 23:15:39 +0100

    Tak

--- Program.cs
+++ Program.cs
@@ -95,62 +95,51 @@ namespace meintro {
         }
 
 
+        static SHM_PROCESS_ENV[] GetSnapshot(String filename) {
+            Console.WriteLine("[*] File '"+filename+"'");
+            FileStream fileStream = new FileStream(filename, FileMode.Open);
+            Debug.Assert(fileStream.Length / Marshal.SizeOf(typeof(SHM_PROCESS_ENV)) == MAX_PROCESS_COUNT);
+            Console.WriteLine("[*] Snapshot len: " + fileStream.Length);
+            Console.WriteLine("[*] Estimated processes: " + fileStream.Length / Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
+            Console.WriteLine("[*]");
+                  
+            SHM_PROCESS_ENV[] snapshot = new SHM_PROCESS_ENV[MAX_PROCESS_COUNT];
+            for (int i = 0; i < MAX_PROCESS_COUNT; i++) {
+                byte[] readBuffer = new byte[Marshal.SizeOf(typeof(SHM_PROCESS_ENV))];
+                BinaryReader reader = new BinaryReader(fileStream);
+                readBuffer = reader.ReadBytes(Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
+                GCHandle handle = GCHandle.Alloc(readBuffer, GCHandleType.Pinned);
+                snapshot[i] = (SHM_PROCESS_ENV)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(SHM_PROCESS_ENV));
+                handle.Free();
+            }
+            return snapshot;
+        }
+
 
         static void Main(string[] args) {
 
+            String directory_log = "C:\\Users\\Stefano\\mepropin";
+            String experiment_log = "date_140904_2141";
+
+            Console.WriteLine("[*] PROCESS_ENV: "   + Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
+            Console.WriteLine("[*] THREAD_ENV:  "   + Marshal.SizeOf(typeof(SHM_THREAD_ENV)));
+            Console.WriteLine("[*] DLL_ENV:     "   + Marshal.SizeOf(typeof(SHM_DLL_ENV)));
+
             try {
 
-                unsafe {
-
-                    var files = Directory.EnumerateFiles("C:\\Users\\Stefano\\mepropin", "*.*", SearchOption.AllDirectories).Where(s => s.StartsWith("date_"));
-                    foreach (var f in files)
-                    {
-                        Console.WriteLine("[*] " + f);
-                    }
-
-
-                    FileStream fileStream = new FileStream(@"c:\Users\Stefano\mepropin\date_140904_1643_s000.log", FileMode.Open);
-
-                    Debug.Assert(fileStream.Length / Marshal.SizeOf(typeof(SHM_PROCESS_ENV)) == MAX_PROCESS_COUNT);
-
-                    Console.WriteLine("[*] Snapshot len: " + fileStream.Length);
-                    Console.WriteLine("[*] Estimated processes: " + fileStream.Length / Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
-                    Console.WriteLine("[*]");
-                    Console.WriteLine("[*] PROCESS_ENV: "   + Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
-                    Console.WriteLine("[*] THREAD_ENV: "    + Marshal.SizeOf(typeof(SHM_THREAD_ENV)));
-                    Console.WriteLine("[*] DLL_ENV: "       + Marshal.SizeOf(typeof(SHM_DLL_ENV)));
-
-                    SHM_PROCESS_ENV[] snapshot = new SHM_PROCESS_ENV[MAX_PROCESS_COUNT];
-                    for (int i = 0; i < MAX_PROCESS_COUNT; i++) {
-                        byte[] readBuffer = new byte[Marshal.SizeOf(typeof(SHM_PROCESS_ENV))];
-                        BinaryReader reader = new BinaryReader(fileStream);
-                        readBuffer = reader.ReadBytes(Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
-                        GCHandle handle = GCHandle.Alloc(readBuffer, GCHandleType.Pinned);
-                        snapshot[i] = (SHM_PROCESS_ENV)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(SHM_PROCESS_ENV));
-                        handle.Free();
-                    }
-
-
-                    
-
-                    /*
-                    Console.WriteLine("[*] Snapshot MAX_DLL_COUNT    " + aStruct.configuration.max_dll_count + " (reader set to " + MAX_DLL_COUNT + ")");
-                    Console.WriteLine("[*] Snapshot MAX_THREAD_COUNT " + aStruct.configuration.max_thread_count + " (reader set to " + MAX_THREAD_COUNT + ")");
-                    Console.WriteLine("[*] Snapshot MAX_CHAR_COUNT   " + aStruct.configuration.max_char_count + " (reader set to " + MAX_CHAR_COUNT + ")");
-
-                    Console.Out.WriteLine("Process name: " + aStruct.process_name);
-                    */
-                    //Console.Out.WriteLine("Test " + Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
-
-                    
-
-                    Console.In.ReadLine();
+                IEnumerable<String> files = Directory.EnumerateFiles(directory_log, "*.*", SearchOption.AllDirectories).Where(s => s.Contains(experiment_log));     
+                List<SHM_PROCESS_ENV[]> experiment = new List<SHM_PROCESS_ENV[]>();
+                foreach (var f in files.OrderBy(s => s)) {
+                    SHM_PROCESS_ENV[] snapshot = GetSnapshot(f);
+                    experiment.Add(snapshot);
                 }
-
+                
             } catch (Exception e) {
                 Console.Out.WriteLine("[*] Exception " + e.Message);
-                Console.In.ReadLine();
             }
+
+            Console.ReadLine();
+
         }
     }
 }
