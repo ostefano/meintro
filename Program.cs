3daf05ec0d0c15668607772daa13a02fb5fa595c
Stefano Ortolani <ostefano@gmail.com>
2014-09-04 17:03:22 +0100

    Lots of progresses

--- Program.cs
+++ Program.cs
@@ -4,290 +4,153 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace meintro {
     class Program {
 
-        private const int ADDRINT_SIZE = 4;
         private const int MAX_CHAR_COUNT = 128;
         private const int MAX_THREAD_COUNT = 64;
         private const int MAX_DLL_COUNT = 256;
-
-        private const int SHM_ADDRINT_SIZE = ADDRINT_SIZE;
-        private const int SHM_INT64_SIZE = sizeof(UInt64);// 8;
-        private const int SHM_INT32_SIZE = sizeof(UInt32);// 4;
-        private const int SHM_INT16_SIZE = sizeof(UInt16);// 2;
-        private const int SHM_CHAR_SIZE = sizeof(bool);//1;
-        
-        private const int SHM_DLL_ENV_SIZE =    (SHM_CHAR_SIZE * MAX_CHAR_COUNT) + 
-                                                (SHM_INT64_SIZE * 6);
-
-        private const int SHM_THREAD_ENV_SIZE = (SHM_INT32_SIZE * 3) + (4) + 
-                                                (SHM_INT64_SIZE * 11) + 
-                                                (SHM_INT16_SIZE * 1) + (6) + 
-                                                (SHM_INT32_SIZE * MAX_DLL_COUNT) + 
-                                                (SHM_DLL_ENV_SIZE * MAX_DLL_COUNT);
-
-        private const int SHM_PROCESS_ENV_SIZE = (SHM_CHAR_SIZE * MAX_CHAR_COUNT) +
-                                                 (SHM_INT32_SIZE * 1) + (4) +
-                                                 (SHM_INT64_SIZE * 1) +
-                                                 (SHM_INT16_SIZE * 1) + (6) +
-                                                 (SHM_INT32_SIZE * MAX_THREAD_COUNT) +
-                                                 (SHM_THREAD_ENV_SIZE * MAX_THREAD_COUNT);
-
-        [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Ansi)]
-        struct SHM_THREAD_ENV {
-            [FieldOffset(0)]
-            public UInt32 thread_id;
-           
-            [FieldOffset(SHM_INT32_SIZE)]
-	        public UInt32  esp_max;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE)]
-	        public UInt32  esp_min;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4)]
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-	        public UInt32[]  code_range;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2))]
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-	        public UInt32[]  data_range;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2))]
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-	        public UInt32[]  stack_range;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 0))]
-	        public UInt64 llstack_counter;			
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 1))]
-	        public UInt64 stack_counter;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 2))]
-	        public UInt64 heap_counter;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 3))]
-	        public UInt64 data_counter;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 4))]
-	        public UInt64 global_slstack_counter;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 5))]
-	        public UInt64 global_stack_counter;	
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 6))]
-	        public UInt64 global_heap_counter;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 7))]
-	        public UInt64 global_data_counter;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 8))]
-	        public UInt16 dll_count;
-     
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 8) + SHM_INT16_SIZE + 6)]
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = MAX_DLL_COUNT)]
-	        public UInt32[] dll_lookup;
-
-            [FieldOffset(SHM_INT32_SIZE + SHM_INT32_SIZE + 4 + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE * 2) + (SHM_INT64_SIZE * 8) + SHM_INT16_SIZE + 6 + (MAX_DLL_COUNT * SHM_INT32_SIZE))]
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U1, SizeConst = SHM_DLL_ENV_SIZE * MAX_DLL_COUNT)]
-	        public Byte[]	dll_envs;
-
-        }
+        private const int MAX_PROCESS_COUNT = 4;
 
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
-        struct SHM_THREAD_ENV_REV
-        {
-            [MarshalAs(UnmanagedType.U4)]
+        struct SHM_THREAD_ENV {
             public UInt32 thread_id;
+            public UInt64 esp_max;
+            public UInt64 esp_min;
 
-            [MarshalAs(UnmanagedType.U4)]
-            public UInt32 esp_max;
+            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U8, SizeConst = 2)]
+            public UInt64[] code_range;
+            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U8, SizeConst = 2)]
+            public UInt64[] data_range;
+            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U8, SizeConst = 2)]
+            public UInt64[] stack_range;
 
-            [MarshalAs(UnmanagedType.U4)]
-            public UInt32 esp_min;
-
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-            public UInt32[] code_range;
-
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-            public UInt32[] data_range;
-
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-            public UInt32[] stack_range;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 llstack_counter;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 stack_counter;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 heap_counter;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 data_counter;
 
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 global_slstack_counter;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 global_stack_counter;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 global_heap_counter;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 global_data_counter;
 
-            [MarshalAs(UnmanagedType.U2)]
             public UInt16 dll_count;
-
             [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = MAX_DLL_COUNT)]
             public UInt32[] dll_lookup;
 
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U1, SizeConst = SHM_DLL_ENV_SIZE * MAX_DLL_COUNT)]
-            public Byte[] dll_envs;
-        }
-
-        [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Ansi)]
-        struct SHM_DLL_ENV { 
-            [FieldOffset(0)]
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_CHAR_COUNT)]
-            public string name;
-
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT))]
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-            public UInt32[] data_range;
-
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT) + (SHM_INT32_SIZE * 2))]
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-            public UInt32[] code_range;
-
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE))]
-            public UInt64 llstack_counter;
-
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE) + SHM_INT64_SIZE)]
-            public UInt64 stack_counter;
-
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE) + SHM_INT64_SIZE + SHM_INT64_SIZE)]
-            public UInt64 heap_counter;
-
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT) + (SHM_INT32_SIZE * 2) + (SHM_INT32_SIZE) + SHM_INT64_SIZE + SHM_INT64_SIZE + SHM_INT64_SIZE)]
-            public UInt64 data_counter;
+            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.Struct, SizeConst = MAX_DLL_COUNT)]
+            public SHM_DLL_ENV[] dll_envs;
         }
 
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
-        struct SHM_DLL_ENV_REV {
+        struct SHM_DLL_ENV {
+
+            public UInt64 dll_id;
+
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_CHAR_COUNT)]
             public string name;
 
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-            public UInt32[] data_range;
+            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U8, SizeConst = 2)]
+            public UInt64[] data_range;
 
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = 2)]
-            public UInt32[] code_range;
+            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U8, SizeConst = 2)]
+            public UInt64[] code_range;
 
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 llstack_counter;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 stack_counter;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 heap_counter;
-
-            [MarshalAs(UnmanagedType.U8)]
             public UInt64 data_counter;
         }
 
-        
-        [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Ansi)]
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        struct SHM_CONFIG_ENV {
+            public UInt16 max_char_count;
+            public UInt16 max_thread_count;
+            public UInt16 max_dll_count;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
         struct SHM_PROCESS_ENV {
-            [FieldOffset(0)]
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_CHAR_COUNT)]
-            public string name;
 
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT))]
+            public SHM_CONFIG_ENV configuration;
+
             public UInt32 process_id;
+            public bool process_arch;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_CHAR_COUNT)]
+            public string process_name;
 
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT) + SHM_INT32_SIZE + 4)]
-            public UInt64 total_counter;
+            public UInt64 timestamp;
+            public UInt64 global_counter;
 
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT) + SHM_INT32_SIZE + 4 + SHM_INT64_SIZE)]
             public UInt16 thread_count;
-
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT) + SHM_INT32_SIZE + 4 + SHM_INT64_SIZE + SHM_INT16_SIZE + 6)]
             [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = MAX_THREAD_COUNT)]
             public UInt32[] thread_lookup;
-
-            [FieldOffset((SHM_CHAR_SIZE * MAX_CHAR_COUNT) + SHM_INT32_SIZE + 4 + SHM_INT64_SIZE + SHM_INT16_SIZE + 6 + (SHM_INT32_SIZE * MAX_THREAD_COUNT))]
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U1, SizeConst = SHM_THREAD_ENV_SIZE * MAX_THREAD_COUNT)]
-            public Byte[] thread_envs;    
+            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.Struct, SizeConst = MAX_THREAD_COUNT)]
+            public SHM_THREAD_ENV[] thread_envs;
         }
 
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
-        struct SHM_PROCESS_ENV_REV {
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_CHAR_COUNT)]
-            public string name;
 
-            [MarshalAs(UnmanagedType.U4)]
-            public UInt32 process_id;
-
-            [MarshalAs(UnmanagedType.U8)]
-            public UInt64 total_counter;
-
-            [MarshalAs(UnmanagedType.U2)]
-            public UInt16 thread_count;
-
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U4, SizeConst = MAX_THREAD_COUNT)]
-            public UInt32[] thread_lookup;
-
-            [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U1, SizeConst = SHM_THREAD_ENV_SIZE * MAX_THREAD_COUNT)]
-            public Byte[] thread_envs;
-        }
 
         static void Main(string[] args) {
 
-            try
-            {
+            try {
 
-                unsafe
-                {
+                unsafe {
 
-                    FileStream fileStream = new FileStream(@"c:\Users\Stefano\mepropin\date_140902_1544_s001.log", FileMode.Open);
+                    var files = Directory.EnumerateFiles("C:\\Users\\Stefano\\mepropin", "*.*", SearchOption.AllDirectories).Where(s => s.StartsWith("date_"));
+                    foreach (var f in files)
+                    {
+                        Console.WriteLine("[*] " + f);
+                    }
 
-                    Console.WriteLine("["+SHM_PROCESS_ENV_SIZE+"] PROCESS_ENV_REV: " + Marshal.SizeOf(typeof(SHM_PROCESS_ENV_REV)) + " PROCESS_ENV: " + Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
-                    Console.WriteLine("["+SHM_THREAD_ENV_SIZE+"] THREAD_ENV_REV: " + Marshal.SizeOf(typeof(SHM_THREAD_ENV_REV)) + " ENV: " + Marshal.SizeOf(typeof(SHM_THREAD_ENV)));
-                    Console.WriteLine("["+SHM_DLL_ENV_SIZE+"] DLL_ENV_REV: " + Marshal.SizeOf(typeof(SHM_DLL_ENV_REV)) + " ENV: " + Marshal.SizeOf(typeof(SHM_DLL_ENV)));
 
-                    SHM_PROCESS_ENV_REV aStruct;
-                    int count = Marshal.SizeOf(typeof(SHM_PROCESS_ENV_REV));
-                    byte[] readBuffer = new byte[count];
-                    BinaryReader reader = new BinaryReader(fileStream);
-                    readBuffer = reader.ReadBytes(count);
-                    GCHandle handle = GCHandle.Alloc(readBuffer, GCHandleType.Pinned);
-                    aStruct = (SHM_PROCESS_ENV_REV)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(SHM_PROCESS_ENV_REV));
+                    FileStream fileStream = new FileStream(@"c:\Users\Stefano\mepropin\date_140904_1643_s000.log", FileMode.Open);
 
-                    Console.Out.WriteLine("Test " + aStruct.name);
+                    Debug.Assert(fileStream.Length / Marshal.SizeOf(typeof(SHM_PROCESS_ENV)) == MAX_PROCESS_COUNT);
 
+                    Console.WriteLine("[*] Snapshot len: " + fileStream.Length);
+                    Console.WriteLine("[*] Estimated processes: " + fileStream.Length / Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
+                    Console.WriteLine("[*]");
+                    Console.WriteLine("[*] PROCESS_ENV: "   + Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
+                    Console.WriteLine("[*] THREAD_ENV: "    + Marshal.SizeOf(typeof(SHM_THREAD_ENV)));
+                    Console.WriteLine("[*] DLL_ENV: "       + Marshal.SizeOf(typeof(SHM_DLL_ENV)));
+
+                    SHM_PROCESS_ENV[] snapshot = new SHM_PROCESS_ENV[MAX_PROCESS_COUNT];
+                    for (int i = 0; i < MAX_PROCESS_COUNT; i++) {
+                        byte[] readBuffer = new byte[Marshal.SizeOf(typeof(SHM_PROCESS_ENV))];
+                        BinaryReader reader = new BinaryReader(fileStream);
+                        readBuffer = reader.ReadBytes(Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
+                        GCHandle handle = GCHandle.Alloc(readBuffer, GCHandleType.Pinned);
+                        snapshot[i] = (SHM_PROCESS_ENV)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(SHM_PROCESS_ENV));
+                        handle.Free();
+                    }
+
+
+                    
+
+                    /*
+                    Console.WriteLine("[*] Snapshot MAX_DLL_COUNT    " + aStruct.configuration.max_dll_count + " (reader set to " + MAX_DLL_COUNT + ")");
+                    Console.WriteLine("[*] Snapshot MAX_THREAD_COUNT " + aStruct.configuration.max_thread_count + " (reader set to " + MAX_THREAD_COUNT + ")");
+                    Console.WriteLine("[*] Snapshot MAX_CHAR_COUNT   " + aStruct.configuration.max_char_count + " (reader set to " + MAX_CHAR_COUNT + ")");
+
+                    Console.Out.WriteLine("Process name: " + aStruct.process_name);
+                    */
                     //Console.Out.WriteLine("Test " + Marshal.SizeOf(typeof(SHM_PROCESS_ENV)));
 
-                    handle.Free();
+                    
 
                     Console.In.ReadLine();
                 }
 
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Console.Out.WriteLine("[*] Exception " + e.Message);
-
                 Console.In.ReadLine();
-
             }
-
         }
     }
 }
